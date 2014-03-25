32acabd5e198de2b32ddd5129083bfc800943e84
Gudny Lara Gudmundsdottir <gudnylara7@gmail.com>
2014-03-25 20:25:21 +0000

    Fyrirbænaskrá með slider, virkar ekki

--- src/is/tru/truin/BaenastundFragment.java
+++ src/is/tru/truin/BaenastundFragment.java
@@ -1,13 +1,17 @@
 package is.tru.truin;
 
 
+import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,33 +19,51 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class BaenastundFragment extends Fragment implements OnClickListener {
+public class BaenastundFragment extends Fragment//implements OnClickListener 
+{
 	
-    Button btnHaldaAfram01;
+    //Button btnHaldaAfram01;
     Context context;
     TextView mannak;
-	
+    ViewPager pager;
+	TruinPagerAdapter mPagerAdapter;
+    
 	public BaenastundFragment(){}
 	
-	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View rootView = inflater.inflate(R.layout.fragment_baenastund, container, false);
+        View rootView = inflater.inflate(R.layout.viewpager_layout, container, false);
         
-        Button btnHaldaAfram01 = (Button) rootView.findViewById(R.id.button_001);
-        btnHaldaAfram01.setOnClickListener(this);  
+     //   Button btnHaldaAfram01 = (Button) rootView.findViewById(R.id.button_001);
+      //  btnHaldaAfram01.setOnClickListener(this);  
 
         mannak = (TextView) rootView.findViewById(R.id.Mannakorn);
         context = getActivity();
         String[] Mannakorn = context.getResources().getStringArray(R.array.mannakorn);
         String randomMannakorn = Mannakorn[new Random().nextInt(Mannakorn.length)];
         
+        pager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        
         mannak.setText(randomMannakorn);
+        
+        this.initialisePaging();
         
         return rootView;
     }
 	
+	private void initialisePaging() {
+		List<Fragment> fragments = new Vector<Fragment>();
+		fragments.add(Fragment.instantiate(getActivity(), BaenastundKyrrdFragment.class.getName()));
+		fragments.add(Fragment.instantiate(getActivity(), BaenastundSignaFragment.class.getName()));
+		fragments.add(Fragment.instantiate(getActivity(), BaenastundOrdGudsFragment.class.getName()));
+		fragments.add(Fragment.instantiate(getActivity(), BaenastundBaeninFragment.class.getName()));
+		this.mPagerAdapter =
+				new TruinPagerAdapter(getActivity().getSupportFragmentManager(), fragments);
+		
+		pager.setAdapter(this.mPagerAdapter);
+	}
+	/*
     @Override
     public void onClick(View v) {
 
@@ -53,6 +75,6 @@ public class BaenastundFragment extends Fragment implements OnClickListener {
 
         transaction.commit(); 
 
-    }
+    }*/
   
 }
---
+++ src/is/tru/truin/TruinPagerAdapter.java
@@ -0,0 +1,31 @@
+package is.tru.truin;
+
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TruinPagerAdapter extends FragmentPagerAdapter{
+
+	private List<android.app.Fragment> fragments;
+	
+	public TruinPagerAdapter(FragmentManager fm, List<android.app.Fragment> fragments) {
+		super(fm);
+		this.fragments = fragments;
+	}
+	
+	@Override
+	public Fragment getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return this.fragments.size();
+	}
+
+	
+}
