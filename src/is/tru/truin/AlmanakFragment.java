9ef2f0817e26cd5f281b02bee9821f829d40decd
snh11 <snh11@hi.is>
2014-03-28 04:21:32 +0000

    Unnid i almanaki

--- src/is/tru/truin/AlmanakFragment.java
+++ src/is/tru/truin/AlmanakFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -16,6 +18,23 @@ public class AlmanakFragment extends Fragment implements OnClickListener {
 	Button Salmabok;
 		
 	public AlmanakFragment(){}
+	
+	Spinner ar;
+	Spinner manudur;
+	Spinner dagur;
+	
+	ArrayAdapter<String> aradapter;
+	ArrayAdapter<String> manuduradapter;
+	ArrayAdapter<CharSequence> daguradapter;
+	
+	
+	String ar_selected = "2005";
+	String manudur_selected = "janúar";
+	String dagur_selected = "1";
+	
+	String [] arValues;
+	String [] manudurValues;
+	String [] dagurValues;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,20 +75,26 @@ public class AlmanakFragment extends Fragment implements OnClickListener {
 	
 	private void setSpinnerContent( View view )
 	{
-        Spinner ar = (Spinner)view.findViewById(R.id.ar); 
-        String [] arValues = {"2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", };
-        ArrayAdapter<String> aradapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, arValues);
+		String [] arValues = {"2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", };
+		String [] manudurValues = {"janúar", "febrúar", "mars", "apríl", "maí", "júní", "júlí", "ágúst", "september", "október", "nóvember","desember", };
+		String [] dagurValues = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31",};
+        
+		ar = (Spinner)view.findViewById(R.id.ar);
+        aradapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, arValues);
         ar.setAdapter( aradapter );
-        
-        Spinner manudur = (Spinner)view.findViewById(R.id.manudur);
-        String [] manudurValues = {"janúar", "febrúar", "mars", "apríl", "maí", "júní", "júlí", "ágúst", "september", "október", "nóvember","desember", };
-        ArrayAdapter<String> manuduradapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, manudurValues);
+        ar.setOnItemSelectedListener(new MyOnItemSelectedListener());
+         
+        manudur = (Spinner)view.findViewById(R.id.manudur);
+        manuduradapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, manudurValues);
         manudur.setAdapter( manuduradapter );
+        manudur.setOnItemSelectedListener(new MyOnItemSelectedListener());
         
-        Spinner dagur = (Spinner)view.findViewById(R.id.dagur);
-        String [] dagurValues = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31",};
-        ArrayAdapter<String> daguradapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, dagurValues);
+        dagur = (Spinner)view.findViewById(R.id.dagur);
+        daguradapter = new ArrayAdapter<CharSequence>(this.getActivity(), android.R.layout.simple_spinner_item, dagurValues);
         dagur.setAdapter( daguradapter );
+        dagur.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        
+
 	}
 
 	@Override
@@ -78,5 +103,41 @@ public class AlmanakFragment extends Fragment implements OnClickListener {
 		
 	}
 	
+	public class MyOnItemSelectedListener implements OnItemSelectedListener{
+
+
+	    public void onItemSelected(AdapterView<?> parent, View v, int pos,long id) {
+		            
+	            if (manudur.getSelectedItem().equals("apríl") || manudur.getSelectedItem().equals("júní") || manudur.getSelectedItem().equals("september") || manudur.getSelectedItem().equals("nóvember")) {
+	            	daguradapter = ArrayAdapter.createFromResource(parent.getContext(), R.array.dagarValuesStuttur,android.R.layout.simple_spinner_item);
+	                daguradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	                dagur.setAdapter(daguradapter);
+	            }
+	            else if (manudur.getSelectedItem().equals("febrúar") && (ar.getSelectedItem().equals("2012") || ar.getSelectedItem().equals("2008") || ar.getSelectedItem().equals("2004"))) {
+	            	daguradapter = ArrayAdapter.createFromResource(parent.getContext(), R.array.dagarValuesFebHlaup,android.R.layout.simple_spinner_item);
+	                daguradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	                dagur.setAdapter(daguradapter);
+	            }
+	            else if (manudur.getSelectedItem().equals("febrúar") && (!ar.getSelectedItem().equals("2012") && !ar.getSelectedItem().equals("2008") && !ar.getSelectedItem().equals("2004"))) {
+	            	daguradapter = ArrayAdapter.createFromResource(parent.getContext(), R.array.dagarValuesFeb,android.R.layout.simple_spinner_item);
+	                daguradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	                dagur.setAdapter(daguradapter);
+	            }
+	            else {
+	            	daguradapter = ArrayAdapter.createFromResource(parent.getContext(), R.array.dagarValues,android.R.layout.simple_spinner_item);
+	                daguradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	                dagur.setAdapter(daguradapter);
+	            }
+
+	    }
+
+
+
+	    public void onNothingSelected(AdapterView<?> arg0) {
+	        // TODO Auto-generated method stub
+
+	    }
+
+	}
 
 }
